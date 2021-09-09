23b4269b54a0a8bda615e81cd939a51390e7284e
Kevin Moore <kevmoo@google.com>
2021-09-08 17:45:43 -0700

    Add `doLog` Object extension

--- app/lib/src/shared.dart
+++ app/lib/src/shared.dart
@@ -1,2 +1,6 @@
 Map<String, String> authHeaders(String bearerToken) =>
     {'Authorization': 'Bearer $bearerToken'};
+
+extension ObjectExt on Object {
+  void doLog(Object? object) => print('$this : $object');
+}
