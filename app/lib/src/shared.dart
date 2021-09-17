5403da463bf9535e6092026670c07a80fba245d0
Kevin Moore <kevmoo@google.com>
2021-09-17 13:46:33 -0700

    app: remove deprecated helper

--- app/lib/src/shared.dart
+++ app/lib/src/shared.dart
@@ -1,10 +1,3 @@
-import 'dart:convert';
-
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:http/http.dart';
-
-import 'network_exception.dart';
-
 const siteTitle = 'Knarly Vote';
 
 Map<String, String> authHeaders(String bearerToken) =>
@@ -12,22 +5,4 @@ Map<String, String> authHeaders(String bearerToken) =>
 
 extension ObjectExt on Object {
   void doLog(Object? object) => print('$this : $object');
-}
-
-@Deprecated('use other')
-Future<Object?> getJson(User user, String url) async {
-  final firebaseIdToken = await user.getIdToken();
-
-  final uri = Uri.parse(url);
-  final response = await get(uri, headers: authHeaders(firebaseIdToken));
-  if (response.statusCode != 200) {
-    throw NetworkException(
-      'Bad response from service! ${response.statusCode}. '
-      '${response.body}',
-      statusCode: response.statusCode,
-      uri: uri,
-    );
-  }
-
-  return jsonDecode(response.body);
 }
